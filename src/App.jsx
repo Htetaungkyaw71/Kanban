f30d4fde40c1c74ff3e6e64f58bf35d66879262e
Htetaungkyaw71 <htetaung200071@gmail.com>
2023-07-20 13:24:59 +0630

    remove project

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import { useState } from "react";
-import { addProject } from "./redux/TaskSlice";
+import { addProject, removeProject } from "./redux/TaskSlice";
 import { useDispatch, useSelector } from "react-redux";
 
 function App() {
   let [name, setName] = useState("");
   let data = useSelector((state) => state.TaskSlice);
+  let keys = Object.keys(data);
   const dispatch = useDispatch();
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -23,6 +24,22 @@ function App() {
         />
         <button type="submit">submit</button>
       </form>
+      <div>
+        {keys.map((i) => (
+          <div key={i}>
+            {i}
+            <button
+              onClick={(e) => {
+                e.preventDefault();
+                dispatch(removeProject(i));
+                console.log(data);
+              }}
+            >
+              Delete
+            </button>
+          </div>
+        ))}
+      </div>
     </>
   );
 }
--- src/redux/TaskSlice.js
+++ src/redux/TaskSlice.js
@@ -7,9 +7,12 @@ const taskSlice = createSlice({
     addProject: (state, action) => {
       state[action.payload] = [];
     },
+    removeProject: (state, action) => {
+      delete state[action.payload];
+    },
   },
 });
 
-export const { addProject } = taskSlice.actions;
+export const { addProject, removeProject } = taskSlice.actions;
 
 export default taskSlice.reducer;
